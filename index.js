894c3a86e564d67e9843e893fcc124130423067a
nasirullahnu <nasirullah878@gmail.com>
2022-11-09 02:35:31 +0600

    backend setup and get services from database

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w5uqgn7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+        try{
+            const serviceCollection = client.db('photography').collection('services')
+
+            // get services from database 
+            app.get('/services' , async (req, res) => {
+                const query = {};
+                const cursor = serviceCollection.find(query);
+                const cursorTwo = serviceCollection.find(query).limit(3)
+                const survices = await cursor.toArray();
+                const serviceHome = await cursorTwo.toArray();
+                res.send({survices, serviceHome})
+            })
+        }
+        finally{
+
+        }
+}
+run().catch(error => console.error(error))
+
+
+
+app.get('/', (req, res)=> {
+    res.send('photography server is running')
+})
+app.listen(port, ()=> {
+    console.log(`Listening to port`, port)
+})
